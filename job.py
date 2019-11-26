50b05acac82aaed0911d0cfdf1730a21da2937e8
Sander Lukas <31885464+sanderlukas@users.noreply.github.com>
2019-11-27 01:42:00 +0200

    Changed the thread type

--- job.py
+++ job.py
@@ -4,7 +4,7 @@ import threading
 class Job(threading.Thread):
     def __init__(self, interval, execute, *args, **kwargs):
         threading.Thread.__init__(self)
-        self.daemon = False
+        self.daemon = True
         self.stopped = threading.Event()
         self.interval = interval
         self.execute = execute
@@ -13,7 +13,7 @@ class Job(threading.Thread):
 
     def stop(self):
         self.stopped.set()
-        self.join()
+        #self.join()
 
     def run(self):
         while not self.stopped.wait(self.interval.total_seconds()):
